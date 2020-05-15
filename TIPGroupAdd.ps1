0ebfea441c856bae25f926f0d20909536d417df3
LeRouteur <47207700+lerouteur@users.noreply.github.com>
2020-05-15 16:28:44 +0200

    Update user registration I added the feature of deleting the user from the DB if the insertion isn't successful. I also updated the PowerShell scripts to check the current group of the user before changing it.

--- TIPGroupAdd.ps1
+++ TIPGroupAdd.ps1
@@ -1,16 +1,22 @@
 ﻿Import-Module ActiveDirectory
 
-$password = ConvertTo-SecureString "" -AsPlainText -Force
-$myCreds = New-Object System.Management.Automation.PSCredential("", $password)
+$password = ConvertTo-SecureString "7BdGYZ8A1vRZo6pH1PpF" -AsPlainText -Force
+$myCreds = New-Object System.Management.Automation.PSCredential("cyril.service", $password)
 
 $groupFile = Get-Content -Path "\\172.16.0.50\tip\group.txt"
 
 if ($groupFile.Count -eq 0) {
+    # File is empty, we exit
     exit;
 } else {
     foreach($line in $groupFile) {
-        $groupData = $groupFile.Split("{,}")
+        $groupData = $line.Split("{,}")
+        Remove-ADGroupMember -Identity "VPNG1" -Members $groupData[1] -Credential $myCreds -Server "172.16.0.51" -Confirm:$false
+        Remove-ADGroupMember -Identity "VPNG2" -Members $groupData[1] -Credential $myCreds -Server "172.16.0.51" -Confirm:$false
+        Remove-ADGroupMember -Identity "VPNG3" -Members $groupData[1] -Credential $myCreds -Server "172.16.0.51" -Confirm:$false
         Add-ADGroupMember -Identity $groupData[0] -Members $groupData[1] -Credential $myCreds -Server "172.16.0.51"
-        Clear-Content -Path "\\172.16.0.50\tip\group.txt"
     }
+    # Clear the content of the file
+    Clear-Content -Path "\\172.16.0.50\tip\group.txt"
+    exit;
 }
